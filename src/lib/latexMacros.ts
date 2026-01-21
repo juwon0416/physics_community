274786ddeb1e754a3ea06a5cb48c0019a8a8871a
juwon0416 <a63924148@gmail.com>
2026-01-21 23:25:26 +0900

    fix(macros): revert to standard pipe | for maximum compatibility while maintaining scaling

--- src/lib/latexMacros.ts
+++ src/lib/latexMacros.ts
@@ -3,9 +3,9 @@
 // ----------------------------------------------------------------------------
 // These will be passed to KaTeX to render them correctly in the preview.
 export const PHYSICS_MACROS = {
-    "\\ket": "\\left\\lvert\\, #1 \\,\\right\\rangle",
-    "\\bra": "\\left\\langle\\, #1 \\,\\right\\rvert",
-    "\\braket": "\\left\\langle\\, #1 \\,\\middle\\vert\\, #2 \\,\\right\\rangle",
+    "\\ket": "\\left|\\, #1 \\,\\right\\rangle",
+    "\\bra": "\\left\\langle\\, #1 \\,\\right|",
+    "\\braket": "\\left\\langle\\, #1 \\,\\middle|\\, #2 \\,\\right\\rangle",
     "\\grad": "\\nabla",
     "\\div": "\\nabla\\cdot",
     "\\curl": "\\nabla\\times",
